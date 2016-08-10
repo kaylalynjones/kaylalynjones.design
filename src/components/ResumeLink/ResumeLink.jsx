30468db14097339fb5ddc41e08c875c95ed90d4c
Kayla Jones <kaylaj@aloompa.com>
2016-08-09 19:39:48 -0500

    tweaked info style added portfolio pdf and change where the resume is stored

--- src/components/ResumeLink/ResumeLink.jsx
+++ src/components/ResumeLink/ResumeLink.jsx
@@ -5,7 +5,7 @@ import './resume-link.scss';
 export default class ResumeLink extends React.Component {
     render() {
         return (
-            <a href="images/kayla-jones-resume.pdf" className="resume-link" target="_blank">
+            <a href="resume.pdf" className="resume-link" target="_blank">
                 Check out my Resume
             </a>
         );
